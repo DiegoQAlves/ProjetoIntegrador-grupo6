bb376f47a7aa8e09da36dfd43635f979d21a71d7
DiegoQAlves <73859566+DiegoQAlves@users.noreply.github.com>
2021-03-18 18:09:18 -0300

    ajustes no express

--- app.js
+++ app.js
@@ -3,12 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-// var methodOverride = require('method-override')
-// var session = require('express-session')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
 
 var app = express();
 
@@ -21,7 +18,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use(methodOverride('_method'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-const indexController = require('../controller/indexController')
-
-// /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
-
-router.get('/', indexController.index)
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
 
 module.exports = router;
